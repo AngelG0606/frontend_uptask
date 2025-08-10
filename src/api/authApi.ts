0c86648547ece12b302365fa0966e23b10c68401
AngelGutierrez06Dev <angel.gutierrez.razo06@gmail.com>
2025-08-10 15:49:37 -0600

    Avance en las vistas de autenticacion

---
+++ src/api/authApi.ts
@@ -0,0 +1,29 @@
+import api from "@/lib/axios";
+import { isAxiosError } from "axios";
+import type { ConfirmToken, UserRegistrationForm } from "../types";
+
+
+
+export async function createAccount (formData : UserRegistrationForm) {
+   try {
+    const url = `/auth/create-account`
+    const { data } = await api.post(url, formData)
+    return data
+   } catch (error) {
+    if(isAxiosError(error) && error.response) {
+        throw new Error(error.response.data.error)
+    }
+   } 
+}
+
+export async function confirmAccount(token : ConfirmToken) {
+    try {
+        const url = `/auth/confirm-account`
+        const { data } = await api.post(url, token)
+        return data
+    } catch (error) {
+        if(isAxiosError(error) && error.response) {
+            throw new Error(error.response.data.error)
+        }
+    } 
+}
