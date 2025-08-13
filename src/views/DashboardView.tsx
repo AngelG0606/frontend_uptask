0d694b16ea0da47978aaec0e9418095e287ffb61
AngelGutierrez06Dev <angel.gutierrez.razo06@gmail.com>
2025-08-13 13:27:01 -0600

    deploy

--- src/views/DashboardView.tsx
+++ src/views/DashboardView.tsx
@@ -29,7 +29,7 @@ export default function DashboardView() {
  if(data && user) return (
     <>
       <h1 className="text-5xl font-black">Mis Proyectos</h1>
-      <p className="text-2xl font-light text-gray-500 mt-5">Maneja y administra tus proyectos  </p>
+      <p className="text-2xl font-light text-gray-500 mt-5">Maneja y Administra tus Proyectos  </p>
 
       <nav className="my-10">
         <Link className="bg-purple-400 hover:bg-purple-500 py-3 px-10 text-white text-xl cursor-pointer font-bold text-center transition-colors"
